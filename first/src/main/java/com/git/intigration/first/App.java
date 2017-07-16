3baa88146600404c04232702c71331945730a8c1
neenareddy <neenamallireddy@gmail.com>
2017-07-16 00:46:35 -0400

    this is the first code in git

---
+++ first/src/main/java/com/git/intigration/first/App.java
@@ -0,0 +1,13 @@
+package com.git.intigration.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
